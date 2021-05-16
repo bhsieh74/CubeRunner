3c7f2efb6b8197f354b1aa8ad1695695996d7583
Brian Hsieh <58496626+bhsieh74@users.noreply.github.com>
2021-05-16 18:40:02 -0400

    Added C# files

---
+++ LevelComplete.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour
+{
+    
+    public void nextLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 5);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(30 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a")) {
+            rb.AddForce(-30 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    
+        if (rb.position.y < -1f) {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
